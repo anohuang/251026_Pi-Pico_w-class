51eb3efe4b55f339b020d489d24ab192cef579c7
ano1207 <ano1019@gmail.com>
2025-12-07 13:48:08 +0800

    modify

--- lesson7/main.py
+++ lesson7/main.py
@@ -4,16 +4,16 @@
 #    print("Hello! pico!")
 #   time.sleep(3)
 
-import wifi_connect
+import wifi_connect as wifi #as 後面接的字可替代前述之function名稱，但下面記得要一併更換
 
 # 嘗試連線 WiFi
-wifi_connect.connect()
+wifi.connect()
 
 # 顯示 IP
-print("IP:", wifi_connect.get_ip())
+print("IP:", wifi.get_ip())
 
 # 測試外部網路
-if wifi_connect.test_internet():
+if wifi.test_internet():
     print("外部網路 OK")
 else:
     print("外部網路無法連線")
