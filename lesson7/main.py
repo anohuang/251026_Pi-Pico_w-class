30474fc50865ffc19f3a1f11b14be82051c55ef1
ano1207 <ano1019@gmail.com>
2025-12-07 14:17:11 +0800

    modify

--- lesson7/main.py
+++ lesson7/main.py
@@ -14,10 +14,29 @@ wifi.connect() #括弧內沒寫的話表示全用預設值
 #混合引述: 若前面有打一個引述名稱的話，後面的也都必須加引述名稱，才不會出錯
 
 # 顯示 IP
-print("IP:", wifi.get_ip())
+#print("IP:", wifi.get_ip())
 
 # 測試外部網路
-if wifi.test_internet():
-    print("外部網路 OK")
-else:
-    print("外部網路無法連線")
+#if wifi.test_internet():
+#    print("外部網路 OK")
+#else:
+#    print("外部網路無法連線")
+import wifi_connect as wifi
+import time
+
+# 嘗試連線 WiFi
+wifi.connect()
+
+# 顯示 IP
+print("IP:", wifi.get_ip())
+
+# 每隔 10 秒測試一次外部網路
+while True:
+    print("-" * 30)
+    if wifi.test_internet():
+        print("外部網路 OK")
+    else:
+        print("外部網路無法連線")
+    
+    print("等待 10 秒後再次測試...")
+    time.sleep(10)
