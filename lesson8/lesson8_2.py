5c8dc225c3a0ac40acde500c5aefc98e88fd5ab4
ano1214 <ano1019@gmail.com>
2025-12-14 15:03:57 +0800

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -5,7 +5,8 @@ btn_pin=14
 rled_pin=15
 yled_pin=13
 
-button=Pin(btn_pin, Pin.IN, Pin.PULL_UP)
+button=Pin(btn_pin, Pin.IN, Pin.PULL_UP) #PULL_UP為上拉電阻>>正電自pin14流出，按鈕按下形成通路流入GND，故電位由1變0
+#PULL_DOWN為下拉電阻>>負電自pin14流入，按鈕按下形成通路流入VCC，故電位由0變1
 rled=Pin(rled_pin, Pin.OUT)
 yled=Pin(yled_pin, Pin.OUT)
 
