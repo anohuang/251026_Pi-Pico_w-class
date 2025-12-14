0a94b419c7f79517c594cb22609bafa5fca71d43
ano1214 <ano1019@gmail.com>
2025-12-14 12:36:00 +0800

    modify

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -1,1 +1,9 @@
-from machine import pin
+from machine import Pin
+import time #也可以寫成from time import sleep#
+
+led_pin=15
+led=Pin(led_pin, Pin.OUT)
+
+while(True):
+    led.toggle()
+    time.sleep(2) #如果按上面註解裡的寫，那此處寫成sleep即可#
