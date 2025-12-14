ec2af778e0e5d5b61b99ff633abef06be40b5490
ano1214 <ano1019@gmail.com>
2025-12-14 13:09:03 +0800

    modify

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -4,6 +4,7 @@ import time #也可以寫成from time import sleep#
 led_pin=15
 led=Pin(led_pin, Pin.OUT)
 
+
 while(True):
     led.toggle()
-    time.sleep(2) #如果按上面註解裡的寫，那此處寫成sleep即可#
+    time.sleep_ms(500)#如果按上面註解裡的寫，那此處寫成sleep即可#
